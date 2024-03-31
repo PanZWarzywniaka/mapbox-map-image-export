b3dc388dd585981fd10aa037045dc972eb7fb02f
Olek Osikowicz <tupolewcx@gmail.com>
2024-03-31 12:40:56 +0100

    Add option to render asyncronously

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var electron = require("electron");
-const { spawnSync } = require('child_process');
+const { spawnSync, spawn } = require('child_process');
 const express = require("express");
 var fs = require('fs')
 const bodyParser = require('body-parser');
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.static(RENDERS_DIR));
 app.use(bodyParser.json());
 
-function renderMapSync(width, height, dpi, bbox) {
+function renderMap(sync, width, height, dpi, bbox) {
 
     const file_name = `map_${width}x${height}@${dpi}_${bbox}.png`
     const args = [
@@ -23,20 +23,35 @@ function renderMapSync(width, height, dpi, bbox) {
         `-b=${bbox}`,
         `-t=${process.env.MAPBOX_TOKEN}`,
         `-o=${RENDERS_DIR}/${file_name}`,
-        `--debug`,
     ];
-    const child = spawnSync(electron, args, { stdio: "inherit" });
-    console.log("Done exporting")
+
+    if (sync) {
+        console.log("Rendering syncronously");
+        const child = spawnSync(electron, args, { stdio: "inherit" });
+    } else {
+        
+        const child = spawn(electron, args, { stdio: "inherit" });
+        console.log("Start exporting to: ", file_name)
+    
+        child.on('close', (code) => {
+            if (code === 0) {
+              console.log("Rendering exited successfully");
+            } else {
+              console.error(`Electron process failed with exit code ${code}`);
+            }
+          });
+    }
+    
     return file_name
 }
 
 app.get("/", (req, res) => {
     const width = 12
-    const height = 15 
+    const height = 15
     const dpi = 96 //288 
     const bbox = '-74.20,40.52,-73.70,41.004'
 
-    const file_name = renderMapSync(width, height, dpi, bbox)
+    const file_name = renderMap(true, width, height, dpi, bbox)
     res.sendFile(`${RENDERS_DIR}/${file_name}`, { root: __dirname });
 });
 
@@ -49,7 +64,7 @@ app.post('/', (req, res) => {
         return res.status(400).send('Missing required parameters: width, height, dpi, bbox');
     }
 
-    const file_name = renderMapSync(width, height, dpi, bbox)
+    const file_name = renderMap(true, width, height, dpi, bbox)
     res.send(file_name)
 });
 
